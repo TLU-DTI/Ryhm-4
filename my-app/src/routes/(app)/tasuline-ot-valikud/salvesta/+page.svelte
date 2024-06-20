bbaa82e287a44a22aa81209bfa9b6266c63638a4
Jarl Koha <146342562+jarkoha@users.noreply.github.com>
2024-06-20 10:24:05 +0300

    fixes

--- my-app/src/routes/(app)/tasuline-ot-valikud/salvesta/+page.svelte
+++ my-app/src/routes/(app)/tasuline-ot-valikud/salvesta/+page.svelte
@@ -1,18 +1,69 @@
-<script>
+<script lang="ts">
     import Button from "$lib/components/Button.svelte";
-    import { tooltip } from "$lib/script/tooltip.js";
     import { goto } from "$app/navigation";
+    import { premiumDecisionStore } from '../../../../store/premiumDecisionStore';
+    import { supabase } from '$lib/supabaseClient';
+    import { get } from "svelte/store";
+    import { sat_user_id } from '../../../../store.js';
+
+    let premiumDecisionData = get(premiumDecisionStore);
+    let userId;
+
+    sat_user_id.subscribe(value => {
+        userId = value;
+    });
+
+    console.log('Retrieved userId:', userId);
+
+    async function saveToDatabase() {
+        const dataToInsert = {
+            choice_name: premiumDecisionData.decisionName,
+            model_type: premiumDecisionData.modelType,
+            choices: premiumDecisionData.choices,
+            criteria: premiumDecisionData.criteria,
+            user_id: userId,
+        };
+
+        // Log the data being sent to the database
+        console.log('Data being sent to the database:', dataToInsert);
+
+        try {
+            const { error } = await supabase
+                .from('premium_decisions')
+                .insert([dataToInsert]);
+
+            if (error) {
+                throw new Error(error.message);
+            } else {
+                console.log('Data saved successfully');
+                // Reset the project store
+                premiumDecisionStore.resetStore();
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+
+    function startAnswering() {
+        saveToDatabase().then(() => goto("/tasuline-ot-valikud/kriteeriumi-vordlus"));
+    }
+
+    function saveResult() {
+        saveToDatabase().then(() => goto("/"));
+    }
+
+    function goBack() {
+        goto("/tasuline-ot-valikud/valikud");
+    }
 </script>
 
 <section class="container">
     <div class="input-container">
-        <h2>Kuidas soovid jätkata?</h2>
-        <br>
-
+        <h2>Mida soovid edasi teha?</h2>
         <div class="buttons">
-            <Button style="secondary" on:click={() => goto("/tasuline-ot-valikud/sisesta-kriteerium")} on:keydown size="large">Tagasi</Button>
-            <Button size="large">Salvesta otsuste tegija ja vasta hiljem</Button>
-            <Button on:click={() => goto("/tasuline-ot-valikud/kriteeriumi-vordlus")} on:keydown size="large">Jätka</Button>
+            <Button on:click={saveResult} on:keydown>Salvesta tulemus</Button>
+            <Button style="secondary" on:click={goBack} on:keydown>Tagasi</Button>
+            <Button on:click={startAnswering} on:keydown>Alusta vastamist</Button>
         </div>
     </div>
 </section>
